2a3222228749c40094fff886327bd36930cf3cce
Noerz <mustaqim.pratama0@gmail.com>
2025-04-18 09:38:08 +0700

    validasi rain chance

--- lib/app/modules/home/widgets/rain_chance_text.dart
+++ lib/app/modules/home/widgets/rain_chance_text.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/app/data/models/weather_model.dart';
 
 class RainChanceText extends StatelessWidget {
   final dynamic weather;
@@ -7,11 +8,20 @@ class RainChanceText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final rainChance = weather.rain?.the1H != null
-        ? (weather.rain!.the1H * 100).toStringAsFixed(0)
-        : '0';
+    String estimateRainChance(Weather weather) {
+      if (weather.rain?.the1H != null && weather.rain!.the1H! > 1.0) {
+        return "90%";
+      } else if (weather.rain?.the1H != null && weather.rain!.the1H! > 0.1) {
+        return "60%";
+      } else if (weather.weather.first.main.toLowerCase().contains("rain")) {
+        return "30%";
+      } else {
+        return "0%";
+      }
+    }
+
     return Text(
-      'Chance of Rain: $rainChance%',
+      'Chance of Rain: ${estimateRainChance(weather)}',
       style: TextStyle(
         color: Colors.white70,
         fontSize: 16,
